b8503c24be29335ac664ec4a196d05b925b8d1ec
Tundzhay Mollov <graculapsone1998@gmail.com>
2019-11-23 18:12:59 +0200

    New look

--- js/app.js
+++ js/app.js
@@ -1,52 +1,17 @@
-var currentPage = 1;
+var currentPage = null;
 
-const pages = [ 
-    null,
+var pages = [
     $("#hello"),
     $("#education"),
     $("#experience"),
     $("#moreExperience"),
-    $("#moreAbout")
-]
+];
 
-const page = {
-    hello: 1,
-    education: 2,
-    experience: 3,
-    moreExperience: 4,
-    projects: 5
-}
-
-$(".fa-chevron-down").click(function () {
-    console.log("Down");
-    currentPage++;
-    if (currentPage > 5) {
-        currentPage = 5;
-    }
-    ShowPage(currentPage);
+$(".fa-arrow-circle-left").click(function () {
+    currentPage = pages.pop();
+    ShowPage(pages[0]);
 });
 
-$(".fa-chevron-up").click(function () {
-    console.log("Up");
-    currentPage--;
-    if (currentPage < 1) {
-        currentPage = 1;
-    }
-    ShowPage(currentPage);
-});
-
-function ShowPage(page) {
-    for (let i = 1; i <= 5; i++) {
-        if (i == page) {
-            pages[i].show();
-            currentPage = i;
-        }
-        else{
-            pages[i].hide();
-        }   
-    }
-}
-
-$(document).ready(function (){
-    ShowPage(page.hello);
+$(".fa-arrow-circle-right").click(function () {
+    
 });
