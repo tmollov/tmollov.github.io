f21abc5a961e19be54c7f2b59eaa30d60b62eb28
Tundzhay Mollov <graculapsone1998@gmail.com>
2022-04-17 21:39:05 +0300

    Updates

--- App/src/models/project.ts
+++
@@ -1,6 +0,0 @@
-export class Project {
-    title: string | undefined;
-    description: string | undefined;
-    image: string | undefined;
-    link: string | undefined
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# tmollov.github.io
